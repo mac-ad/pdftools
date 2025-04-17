290d5567e7facd11f4348159b9e3775046e4d9e5
mac-ad <macad626@gmail.com>
2025-04-17 18:04:08 +0545

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Providers from "@/Context/Providers";
+import { Toaster } from "sonner";
+import Script from "next/script";
+import Analytics from "@/components/Analytics/Analytics";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -51,25 +54,26 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <Analytics />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <script type="application/ld+json" dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            "@context": "https://schema.org",
-            "@type": "WebApplication",
-            "name": "PDF Toolbox",
-            "url": process.env.NEXT_PUBLIC_DOMAIN,
-            "description": "Free and powerful PDF tools to merge, convert, compress, and edit PDFs online.",
-            "applicationCategory": "Utility",
-            "operatingSystem": "All",
-            "browserRequirements": "Requires JavaScript",
-            "offers": {
-              "@type": "Offer",
-              "price": "0",
-              "priceCurrency": "USD"
-            }
-        })
-}} />
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebApplication",
+              "name": "PDF Toolbox",
+              "url": process.env.NEXT_PUBLIC_DOMAIN,
+              "description": "Free and powerful PDF tools to merge, convert, compress, and edit PDFs online.",
+              "applicationCategory": "Utility",
+              "operatingSystem": "All",
+              "browserRequirements": "Requires JavaScript",
+              "offers": {
+                "@type": "Offer",
+                "price": "0",
+                "priceCurrency": "USD"
+              }
+          })
+        }} />
 
       </head>
       <body
@@ -77,6 +81,7 @@ export default function RootLayout({
       >
          <Providers>
             <div className="relative">
+              <Toaster richColors/>
               <Header />
               <main className="mt-16">
                 {children}
